a20065faa9a7eba47f05c3530c3788429dba052d
christien07 <christien0101@protonmail.com>
2022-01-14 20:17:03 -0700

    Add playRPS function

--- game.js
+++ game.js
@@ -13,4 +13,27 @@ function computerPlay() {
     }
 }
 
-computerPlay();
+prompt("Rock, Paper, or Scissors?");
+
+function playRPS(playerSelection, computerSelection) {
+    let playerSelection;
+    let computerSelection = computerPlay();
+
+    if(playerSelection === computerSelection) {
+        return "Tie."
+    } else if(playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return "You Lose! Paper beats Rock";
+    } else if(playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return "You Win! Rock beats Scissors";
+    } else if(playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return "You Win! Paper beats Rock";
+    } else if(playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return "You Lose! Scissors beats Paper";
+    } else if(playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return 'You Lose! Rock beats Scissors';
+    } else if(playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return "You Win! Paper beats Scissors";
+    } else {
+        return "Please select a valid option for the game";
+    }
+}
